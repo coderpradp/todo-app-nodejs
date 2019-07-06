9d88fcf6905f11e7a0ab0d45bf15f8c6e23f0279
Pradip Khadka <fnfdrifter@gmail.com>
2019-07-06 18:04:39 +0545

    Mongoose/MongoDB connection setup

--- server/index.js
+++ server/index.js
@@ -2,8 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.Promise = global.Promise
+// Connecting to MongoDB Atlas
+mongoose
+  .connect(process.env.CONNECTION_STRING, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB server')
+  })
+  .catch(err => {
+    console.log('Failed to connect to MongoDB server')
+    console.log(err)
+  })
 
 app.use(bodyParser.json())
 app.use(cors())
